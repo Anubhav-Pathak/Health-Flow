81663ac5c438090654733652bb4905f40e9b60ae
vaishnav-mk <vaishnav240204@gmail.com>
2024-03-04 13:39:22 +0530

    fix

--- src/components/ui/Input.jsx
+++ src/components/ui/Input.jsx
@@ -1,15 +1,16 @@
-import React from "react";
+import React, { forwardRef } from 'react';
 
-const Input = React.forwardRef(
-  ({ style, label, input, error }, ref) => {
+const Input = forwardRef((props, ref) => {
     return (
-      <div className={style}>
-        {label && <label htmlFor={input.id}>{label}</label>}
-        <input ref={ref} {...input} />
-        {error && <p className="text-red-500 mt-2">{error}</p>}
-      </div>
+        <label className={`form-control w-full ${props.styles}`}>
+            <div className="label">
+                <span className="label-text">{props.label}</span>
+            </div>
+            <input ref={ref} type={props.type} className="input input-bordered w-full"/>
+        </label>
     );
-  }
-);
+});
+
+Input.displayName = 'Input';
 
 export default Input;
